9f1d0c32a31cb1b70e9d7fc824e9f9d3dfff94fa
Thazar-r <melchior.balthazar@student.moringaschool.com>
2024-06-23 23:46:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('Button was clicked!');
+    }
+  
+    button.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener();
+  
